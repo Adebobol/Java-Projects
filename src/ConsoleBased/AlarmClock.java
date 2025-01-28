4a6a0aca86cac5302dfbbe64d0cfc59d4ca8fdbe
Adebobola <adepojuadebobola6@gmail.com>
2025-01-28 22:35:25 +0100

    errors: while loop and timer probs.

--- src/ConsoleBased/AlarmClock.java
+++ src/ConsoleBased/AlarmClock.java
@@ -1,8 +1,11 @@
 package ConsoleBased;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class AlarmClock {
     public static void main(String[] args) {
@@ -15,13 +18,15 @@ public class AlarmClock {
 
         Scanner scanner =  new Scanner(System.in);
 
-        // present factors
-        LocalDateTime date = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d,MMMM uuuu.");
-        // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d, MMMM uuuu. HH:mm");
-        // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
-        String presentDate = date.format(formatter);
-        System.out.print(presentDate);
+        // present date
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d,MMMM uuuu.");
+        String presentDate = date.format(dateFormatter);
+        // present date and time
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d,MMMM uuuu. HH:mm");
+        String presentDateTime = dateTime.format(dateTimeFormatter);
+        System.out.println(presentDateTime);
         System.out.println();
         
 
@@ -31,6 +36,9 @@ public class AlarmClock {
         String uuuu = "";
         String HH = "";
         String mm = "";
+        String setAlarm = "";
+   
+        boolean on = true;
 
         // response variable for switch
         String response = "";
@@ -48,7 +56,10 @@ public class AlarmClock {
                 System.out.println("Enter mm: ");
                 mm = scanner.next();
                 // Alarm set for 08:30. Waiting...
+                setAlarm = presentDate+" "+HH+":"+mm;
+                System.out.println(setAlarm);
                 System.out.printf("Alarm set for: %s %s:%s\nwaiting...", presentDate,HH,mm);
+                
                 break;
             case "2":
                 System.out.println("Enter HH: ");
@@ -61,10 +72,36 @@ public class AlarmClock {
                 MMMM = scanner.next();
                 System.out.println("Enter year: ");
                 uuuu = scanner.next();
+                setAlarm = d+","+MMMM+ " " +uuuu+". " + HH+":"+mm;
+                System.out.println(setAlarm);
                 System.out.printf("Alarm for: %s,%s %s. %s:%s",d,MMMM,uuuu,HH,mm);
                 break;
             default:
                 System.out.println("Wrong inputs.");
+        }
+
+        while(on){
+          Timer timer = new Timer();
+          TimerTask task = new TimerTask() {
+
+
+            @Override
+            public void run() {
+                String presentDateTime ="";
+                // present date and time
+            LocalDateTime dateTime11 = LocalDateTime.now();
+            DateTimeFormatter dateTime11Formatter = DateTimeFormatter.ofPattern("d,MMMM uuuu. HH:mm");
+            presentDateTime = dateTime11.format(dateTime11Formatter);
+
+            System.out.println(presentDateTime);
+                System.out.println(setAlarm);
+
+            if(presentDate.equals(setAlarm)) {
+                System.out.println("Alarm rings!!!");
+            }
+            }
+            
+          };
         }
 
         scanner.close();
