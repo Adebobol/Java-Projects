8cb1c5020bffbeadb8478d22b49567be3fd75e6b
Adebobola <adepojuadebobola6@gmail.com>
2025-01-31 22:45:54 +0100

    feat: init ussd code.

---
+++ src/USSD/Ussd.java
@@ -0,0 +1,56 @@
+package USSD;
+
+import java.util.Scanner;
+
+// class USSDKOMPANY{
+//     private
+// }
+
+public class Ussd {
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // necessary variables
+        String ussdCode = "";
+        boolean running = false;
+        String serviceResponse = "";
+
+
+
+        // try-catch
+        try {
+            System.out.println("Ussd: ");
+            ussdCode = scanner.nextLine();
+            System.out.println(ussdCode);
+            if(ussdCode.equals("*996#")){
+                running = true;
+            } else {
+                System.out.println("Invalid code.");
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
+
+        do{
+            System.err.println("Eazybanking");
+            System.out.println("Open Account");
+            System.out.println("1>Airtime");
+            System.out.println("2>Data");
+            System.out.println("4>Transfer");
+            System.out.println("5>Check Balance");
+
+            System.out.println("Response: ");
+            serviceResponse= scanner.nextLine();
+
+            switch(serviceResponse){
+                case "1"->System.out.println();
+                
+            }
+        }while(running);
+
+        scanner.close();
+
+   }
+    
+}
