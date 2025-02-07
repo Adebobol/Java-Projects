7e50e2f452d893e7169488feb685b71dfe5ec3b8
Adebobola <adepojuadebobola6@gmail.com>
2025-02-07 16:27:26 +0100

    feat: added getTask() and saveTask() to Task Manager methods.

--- src/TaskManagement/TaskManager.java
+++ src/TaskManagement/TaskManager.java
@@ -1,32 +1,66 @@
 package TaskManagement;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class TaskManager{
-    ArrayList<Task> tasks; 
+    private ArrayList<Task> tasks; 
+    private static final String File_Name = "Mytasks.txt";
 
 
 
     public void createTask(String title, String description, LocalDate createdDate, boolean isDone){
-        if(title==" " || description==" "){
-            System.out.println("Title can't be empty.");
+       if(title!="" && description!=""){
+            int id = tasks.size() + 1;
+            new Task(id, title, description, createdDate, isDone, createdDate);
+            saveTask();
+       }
+    }
+
+    public Task getTask(int id){
+        Task task = new Task(id, null, null, null, false, null);
+        if (id > tasks.size()) {
+            System.out.println("Invalid id.");
+        } else {
+            tasks.get(id);
         }
-        int currentSize = tasks.size();
-        int id = currentSize + 1;
-        tasks.add(new Task(id, title, description, createdDate, isDone, createdDate));
+        return task;
     }
 
-    public void getTask(int id){
-        tasks.get(id);
+    public Task updateTask(int id,String title, String description,LocalDate toBeCompleted){
+        Task updatedTask = new Task(id, title, description, toBeCompleted, false, toBeCompleted);
+        try {
+            if(tasks.size()<id){
+                System.out.println("Invalid id.");
+            } else {
+                Task newTask = new Task(id, title, description, null, false, toBeCompleted);
+                updatedTask=tasks.set(id,newTask);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
+        saveTask();
+       return updatedTask;
     }
 
-    public String updateTask(int id){
-        
-        return "";  
+    public void deleteTask(int id){
+        tasks.remove(id);
+        saveTask();
+        System.err.print("Task deleted from cache.");
     }
 
-    public String deleteTask(){
-        return "";
+    private void saveTask(){
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(File_Name))) {
+            oos.writeObject(tasks);
+        } catch (IOException e) {
+            // TODO: handle exception
+            System.out.println("File not found.");
+            e.printStackTrace();
+        }
     }
 }
