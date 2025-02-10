017bcd02e648dabb2b35256fb3a4689a48864d76
Adebobola <adepojuadebobola6@gmail.com>
2025-02-10 11:59:06 +0100

    chat system fixes. Still haven't got the application

---
+++ src/ChatSystem/ChatClient.java
@@ -0,0 +1,44 @@
+package ChatSystem;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ChatClient {
+    private Socket socket = null;
+
+
+    public void ChatC(String Ip, int Port ){
+        try {
+            socket = new Socket(Ip,Port);
+            System.out.println("Connecting to the server...");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    public void sendMessage(String msg) throws IOException{
+        System.out.println("sending message to server");
+        PrintWriter output = new PrintWriter(socket.getOutputStream());
+        output.println(msg);
+    }
+
+    
+    public void close() throws IOException{
+        if(socket !=null) {
+            socket.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        ChatClient client = new ChatClient();
+        try {
+            client.ChatC("127.0.0.1", 4000);
+            client.sendMessage("hello");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
--- src/ChatSystem/ChatServer.java
+++ src/ChatSystem/ChatServer.java
@@ -1,7 +1,10 @@
 package ChatSystem;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,17 +13,43 @@ public class ChatServer {
     ServerSocket server = null;
     DataInputStream input = null;
 
-    public ChatServer(int Port){
+    public void ChatS(int Port){
         try {
             server = new ServerSocket(Port);
             System.out.println("server started.");
             System.out.println("waiting for a client to connect ...");
-            input = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            // input = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            // server.accept();
+            while(true){
+                server.accept();
+                System.out.println("client connected.");
+                readMessages(socket);
+            }
         } catch (Exception e) {
             // TODO: handle exception
         }
     }
+
+    public void readMessages(Socket clientSocket) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        // reader.read();
+        // reader.ready();
+        for(int chr = reader.read(); reader.ready(); chr = reader.read()) {
+            System.out.println((char) chr);
+        }
+    }
+
+    public void close() throws IOException{
+        if(server !=null) {
+            server.close();
+        } 
+    }
+
     public static void main(String[] args) {
-        
+       ChatServer server = new ChatServer();
+       
+        server.ChatS(4000);
+       
     }
 }
