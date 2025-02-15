bf72f804840b70f16c7d3c342c1316461f113de0
Adebobola <adepojuadebobola6@gmail.com>
2025-02-15 01:22:34 +0100

    error handling and closing of streams to avoid leakages.

--- src/MessagingSystem/ClientConnected.java
+++ src/MessagingSystem/ClientConnected.java
@@ -1,30 +1,52 @@
 package MessagingSystem;
 
+import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.Socket;
 
-public class ClientConnected {
+public class ClientConnected implements Runnable {
     private Socket clients;
     private DataInputStream input;
-    public ClientConnected(Socket clients){
+    private int clientId;
+    public ClientConnected(Socket clients, int clientId){
         this.clients= clients;
+        this.clientId= clientId;
         try {
-            this.input = new DataInputStream(this.clients.getInputStream());
+            this.input = new DataInputStream( new BufferedInputStream(this.clients.getInputStream()));
         } catch (IOException e) {
             e.printStackTrace();
+            close();
+        }
+    }
+
+    @Override
+    public void run() {
+        readMessages();
+        
+    }
+
+    public void close(){
+        try {
+            if(clients!=null) clients.close();
+            if(input!= null) input.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 
     public void readMessages(){
         String message="";
-        while(!message.equals("over")){
-            try {
-                message = input.readUTF();
-            } catch (Exception e) {
-                // TODO: handle exception
-            }
-            System.out.println(message);
+       try {
+         while(!message.equals("over")){
+            message = input.readUTF();
+            System.out.printf("message from client %d: %s", clientId,message);
+            System.out.println();
         }
+       } catch (IOException e) {
+            System.out.println(e.getMessage());
+       } finally {
+            close();
+       }
     }
 }
--- src/MessagingSystem/ServerSystem.java
+++ src/MessagingSystem/ServerSystem.java
@@ -5,8 +5,8 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
-import javax.xml.crypto.Data;
 
 public class ServerSystem {
     String Ip = "127.0.0.1";
@@ -14,36 +14,54 @@ public class ServerSystem {
     private ServerSocket server = null;
     private Socket socket = null;
     private DataInputStream input = null;
+    private int id;
+    ArrayList<Socket> clients = new ArrayList<>();
 
     public void LinkServer(){
         
             try {
-                server = new ServerSocket(Port);
+                server = new ServerSocket(Port, 2);
                 System.out.println("waiting for client to connect.");
                 while(true){ 
                     socket=server.accept();
-                    System.out.println("client connected.   ");
-
+                    clients.add(socket);
+                    id++;
+                    System.out.printf("client %d connected.   ",id);
+                    System.out.println();
                     if(socket.isConnected())
                     new Thread(()->{
-                        ClientConnected client = new ClientConnected(socket);
+                        ClientConnected client = new ClientConnected(socket,id);
                         client.readMessages();
                     }).start();
-
+                    if(!socket.isConnected()) break;
                 }
-            } catch (Exception e) {
-                
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            } finally {
+                close();
             }
         
     }
 
-    private void readMessage() throws IOException{
-        String line = "";
-        while(!line.equals("cancel")){
-            line = input.readUTF();
-            System.out.println(line);
+    public void close(){
+        try {
+            if(server!=null) server.close();
+            if(input!= null) input.close();
+            for(Socket client: clients) {
+                if(client != null) client.close();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
+
+    // private void readMessage() throws IOException{
+    //     String line = "";
+    //     while(!line.equals("cancel")){
+    //         line = input.readUTF();
+    //         System.out.println(line);
+    //     }
+    // }
     public static void main(String[] args) {
         ServerSystem serverSys = new ServerSystem();   
         serverSys.LinkServer();
